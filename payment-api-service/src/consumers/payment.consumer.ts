f388db00e01ae5b54e685406fa9f0853eb2ac0d2
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2024-07-20 00:25:33 -0300

    feat: payment consumer

---
+++ payment-api-service/src/consumers/payment.consumer.ts
@@ -0,0 +1,25 @@
+import { kafkaConsumer } from "../configs/kafka.consumer";
+import { PaymentService } from "../services/PaymentService";
+
+type PaymentMessage = {
+    userEmail: string;
+    amount: number;
+};
+
+export async function paymentConsumer() {
+    const consumer = await kafkaConsumer("order-payment");
+    await consumer.run({
+        eachMessage: async ({ message }) => {
+            const toString = message.value!.toString();
+            const payment = JSON.parse(toString) as PaymentMessage;
+
+            console.log(payment);
+            
+
+            const paymentService = new PaymentService();
+
+            await paymentService.post(payment);
+    }});
+}
+
+paymentConsumer();
