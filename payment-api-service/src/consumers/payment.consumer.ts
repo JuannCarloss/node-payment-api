ed568d219eea44213fcabdc6792abbf6c6cee88d
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2024-07-28 16:31:21 -0300

    feat: sending messages with producer

--- payment-api-service/src/consumers/payment.consumer.ts
+++ payment-api-service/src/consumers/payment.consumer.ts
@@ -1,25 +1,61 @@
 import { kafkaConsumer } from "../configs/kafka.consumer";
+import { KafkaSendMessage } from "../configs/kafka.producer";
+import ValidationException from "../middleware/ValidationException";
 import { PaymentService } from "../services/PaymentService";
 
-type PaymentMessage = {
+interface Payment {
     userEmail: string;
     amount: number;
 };
+
+type Message = {
+    email: string;
+    date: string;
+    amount: number;
+}
+
+interface PaymentResponse {
+    email: string;
+    status: string;
+    date: string;
+    total: number;
+}
+
+function NewResponse(email: string, status: string, date: string, amount: number): PaymentResponse {
+    var response: PaymentResponse = {
+        email: email,
+        status: status,
+        date: date,
+        total: amount
+    };
+    return response;
+}
 
 export async function paymentConsumer() {
     const consumer = await kafkaConsumer("payment");
     await consumer.run({
         eachMessage: async ({ message }) => {
             const toString = message.value!.toString();
-            const payment = JSON.parse(toString) as PaymentMessage;
+            const messages = JSON.parse(toString) as Message;
 
-            console.log(payment);
-            
+            var payment: Payment = {
+                userEmail: messages.email,
+                amount: messages.amount
+            }
 
             const paymentService = new PaymentService();
-
-            await paymentService.post(payment);
-    }});
+            const response = new KafkaSendMessage();
+            try {
+                await paymentService.post(payment);
+                await response.execute(NewResponse(messages.email, "SUCCESSFUL_PAYMENT", messages.date, payment.amount))
+            }catch(e){
+                if (e){
+                    await response.execute(NewResponse(messages.email, "UNSUCCESSFUL_PAYMENT", messages.date, payment.amount))
+                }
+            }
+            
+        }
+    });
 }
 
 paymentConsumer();
